5563d1b63105827c38b1bf29c38f4c3d813b97c2
markramlich <krme23gb@student.ju.se>
2025-10-14 11:50:59 +0200

    feat(project): add mountains

--- project.js
+++ project.js
@@ -98,6 +98,7 @@ function drawSkyScrolling() {
   if (yScroll < maxYScroll) {
     translate(0, -yScroll);
     drawSquares();
+    drawGround();
     drawFields();
 
     let cloudYOffset = yScroll - 1200;
@@ -107,12 +108,18 @@ function drawSkyScrolling() {
   } else {
     translate(-hScroll, -maxYScroll);
     drawSquares();
+    drawGround();
     drawFields();
 
     image(cloudLayer, -hScroll, -(maxYScroll - 1200));
   }
   image(starLayer, 0, 0);
   pop();
+}
+
+function drawGround() {
+  drawMountain();
+  drawFields();
 }
 
 function drawSquares() {
@@ -147,6 +154,71 @@ function drawSquares() {
     }
   }
 }
+
+function drawMountain() {
+  const baseY = height + maxYScroll - 1000;
+  const width = 800;
+  noStroke();
+
+  fill(30, 50, 100);
+  beginShape();
+  vertex(0, baseY + 550);
+  vertex(width * 0.1, baseY + 320);
+  vertex(width * 0.25, baseY + 420);
+  vertex(width * 0.4, baseY + 180);
+  vertex(width * 0.5, baseY + 310);
+  vertex(width * 0.6, baseY + 230);
+  vertex(width * 0.75, baseY + 400);
+  vertex(width * 0.79, baseY + 350);
+  vertex(width * 1.8, baseY + 1500);
+  vertex(width, height + maxYScroll);
+  vertex(0, height + maxYScroll);
+  endShape(CLOSE);
+
+  fill(40, 70, 130);
+  beginShape();
+  vertex(0, baseY + 650);
+  vertex(width * 0.08, baseY + 470);
+  vertex(width * 0.22, baseY + 560);
+  vertex(width * 0.35, baseY + 400);
+  vertex(width * 0.5, baseY + 520);
+  vertex(width * 0.58, baseY + 360);
+  vertex(width * 0.8, baseY + 570);
+  vertex(width * 0.8, baseY + 510);
+  vertex(width * 1.7, baseY + 1500);
+  vertex(width, height + maxYScroll);
+  vertex(0, height + maxYScroll);
+  endShape(CLOSE);
+
+  fill(60, 100, 170);
+  beginShape();
+  vertex(0, baseY + 720);
+  vertex(width * 0.12, baseY + 670);
+  vertex(width * 0.28, baseY + 720);
+  vertex(width * 0.45, baseY + 560);
+  vertex(width * 0.55, baseY + 660);
+  vertex(width * 0.65, baseY + 560);
+  vertex(width * 0.85, baseY + 700);
+  vertex(width * 0.87, baseY + 670);
+  vertex(width * 1.6, baseY + 1500);
+  vertex(width, height + maxYScroll);
+  vertex(0, height + maxYScroll);
+  endShape(CLOSE);
+
+  fill(90, 140, 210);
+  beginShape();
+  vertex(0, baseY + 780);
+  vertex(width * 0.15, baseY + 740);
+  vertex(width * 0.3, baseY + 800);
+  vertex(width * 0.5, baseY + 700);
+  vertex(width * 0.6, baseY + 790);
+  vertex(width * 0.73, baseY + 720);
+  vertex(width * 1.4, baseY + 1500);
+  vertex(width, height + maxYScroll);
+  vertex(0, height + maxYScroll);
+  endShape(CLOSE);
+}
+
 function drawFields() {
   const fieldColors = [
     color(44, 76, 59),
